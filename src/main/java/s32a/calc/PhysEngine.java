7ce2cd034c47dac8f87f35d01f5652bacc2f4c03
Kargathia <kargathia@hotmail.com>
2014-12-18 15:54:41 +0100

    maven project publish

---
+++ src/main/java/s32a/calc/PhysEngine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s32a.calc;
+
+import org.jbox2d.common.*;
+
+/**
+ *
+ * @author Kargathia
+ */
+public class PhysEngine {
+
+    public PhysEngine() {
+        
+    }
+    
+    
+}
